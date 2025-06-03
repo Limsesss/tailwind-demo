ae53db92d6c30cdd2a3ea244068dbc2da5705291
Limsesss <taskinen2005@gmail.com>
2025-06-03 22:22:27 +0300

    idsdfsdf

--- client/src/vite.config.ts
+++ client/src/vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './client',
+  root: '.', // корень — текущая папка client
   plugins: [react()],
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
   },
+  base: './', // для правильных путей в билде
   server: {
     open: true, 
   },
